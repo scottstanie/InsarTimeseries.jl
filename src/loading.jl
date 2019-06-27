aad669348ef2839d098cb2cce71ab4bb7c49154b
scott <scott.stanie@gmail.com>
2019-06-27 13:15:33 -0500

    make the load function work for non-complex

--- src/loading.jl
+++ src/loading.jl
@@ -1,4 +1,67 @@
 import Glob
+using PyCall
+
+
+const SENTINEL_EXTS = [".geo", ".cc", ".int", ".amp", ".unw", ".unwflat"]
+const COMPLEX_EXTS = [".int", ".slc", ".geo", ".cc", ".unw", ".unwflat", ".mlc", ".grd"]
+const REAL_EXTS = [".amp", ".cor", ".mlc", ".grd"]
+const ELEVATION_EXTS = [".dem", ".hgt"]
+# These file types are not simple complex matrices: see load_stacked_img for detail
+# .unwflat are same as .unw, but with a linear ramp removed
+const STACKED_FILES = [".cc", ".unw", ".unwflat"]
+const IMAGE_EXTS = [".png", ".tif", ".tiff", ".jpg"]
+
+const LOAD_IN_PYTHON = vcat(ELEVATION_EXTS, IMAGE_EXTS, [".rsc", ".geojson", ".npy"])
+
+const sario = PyNULL()
+
+function __init__()
+	copy!(sario, pyimport("apertools.sario"))
+end
+
+"""Examines file type for real/complex and runs appropriate load
+
+Raises:
+	ValueError: if sentinel files loaded without a .rsc file in same path
+		to give the file width
+"""
+function load(filename::String; rsc_file::Union{String, Nothing}=nothing)
+    ext = get_file_ext(filename)
+
+	# For now, just pass through unimplemented extensions to Python
+	if ext in LOAD_IN_PYTHON
+		return sario.load(filename)
+	end
+
+
+    # Sentinel files should have .rsc file: check for dem.rsc, or elevation.rsc
+    rsc_data = nothing
+	if !isnothing(rsc_file)
+		rsc_data = sario.load(rsc_file)::Dict{String, Any}
+	end
+
+    if ext in SENTINEL_EXTS
+		if isnothing(rsc_file)
+			rsc_file = sario.find_rsc_file(filename)
+		end
+        rsc_data = sario.load(rsc_file)
+	end
+
+    if ext in STACKED_FILES
+        return load_stacked_img(filename, rsc_data)
+	end
+    # having rsc_data implies that this is not a UAVSAR file, so is complex
+	# TODO: haven"t transferred over UAVSAR functions, so no load_real yet
+	return load_complex(filename, rsc_data=rsc_data)
+
+end
+
+# # Make a shorter alias for load_file
+# load = load_file
+
+"""Extracts the file extension, including the "." (e.g.: .slc)"""
+get_file_ext(filename::String) = splitext(filename)[end]
+
 
 function read_stack(file_ext::String, rows::Int, cols::Int)
 	allfiles = Glob.glob(file_ext, ".")
@@ -10,3 +73,4 @@ function read_stack(file_ext::String, rows::Int, cols::Int)
     end
     return stack
 end
+
---
+++ src/timeseries.jl
@@ -0,0 +1,101 @@
+using Dates
+
+
+"""Finds the number of days between successive .geo files"""
+function day_diffs(geolist::Array{Date, 1})
+	[difference.value for difference in diff(geolist)]
+end
+
+
+
+"""
+	matrixA(geolist::Array{Date, 1}, intlist::Array{Tuple{Date, Date}, 1}) -> Array{Int, 2}
+
+Takes the list of igram dates and builds the SBAS A matrix
+
+Returns the incident-like matrix from the SBAS paper: A*phi = dphi
+	Each row corresponds to an igram, each column to a .geo
+	value will be -1 on the early (slave) igrams, +1 on later (master)
+"""
+function matrixA(geolist, intlist)
+    # We take the first .geo to be time 0, leave out of matrix
+    # Match on date (not time) to find indices
+	geolist = geolist[2:end]
+
+	M = length(intlist)  # Number of igrams, 
+	N = length(geolist)
+	A = zeros(Int8, M, N)
+	for j in 1:M
+		early_date, late_date = intlist[j]
+		idx_early = findfirst(isequal(early_date), geolist)
+		if ~isnothing(idx_early)  # The first SLC will not be in the matrix
+			A[j, idx_early] = -1
+		end
+		idx_late = findfirst(isequal(late_date), geolist)
+        A[j, idx_late] = 1
+
+	end
+	A
+end
+
+
+"""Takes SBAS velocity output and finds phases
+
+Arguments:
+	velocities come from invert_sbas
+	timediffs are the days between each SAR acquisitions
+		length will be 1 less than num SAR acquisitions
+"""
+function integrate_velocities(velocities::Array{Float64, 1}, timediffs::Array{Int, 1})
+    # multiply each column of vel array: each col is a separate solution
+    phi_diffs = velocities .* td_ints
+
+    # Now the final phase results are the cumulative sum of delta phis
+	# This is equivalent to replacing missing with 0s (like np.ma.cumsum does)
+	phi_arr = cumsum(coalesce.(phi_diffs, 0))
+
+    # Add 0 as first entry of phase array to match geolist length on each col
+	pushfirst!(phi_arr, 0)
+
+    phi_arr
+end
+
+
+
+
+
+
+# FUTURE TEST:
+# geolist = collect(Date(2019,1,1):Day(2):Date(2019,1,7))
+# intlist = []
+# 
+# for i in 1:length(geolist)-1
+# 	for j in i+1:length(geolist)
+# 		push!(intlist,(geolist[i], geolist[j]))
+# 	end
+# end
+#
+# julia> geolist
+# #4-element Array{Date,1}:
+#  2019-01-01
+#  2019-01-03
+#  2019-01-05
+#  2019-01-07
+# julia> intlist
+# 6-element Array{Any,1}:
+#  (2019-01-01, 2019-01-03)
+#  (2019-01-01, 2019-01-05)
+#  (2019-01-01, 2019-01-07)
+#  (2019-01-03, 2019-01-05)
+# (2019-01-03, 2019-01-07)
+# (2019-01-05, 2019-01-07)
+#
+# A = InsarTimeseries.matrixA(geolist, intlist)
+# julia> A = InsarTimeseries.matrixA(geolist, intlist)
+# 6×3 Array{Int8,2}:
+#   1   0  0
+#   0   1  0
+#   0   0  1
+#  -1   1  0
+#  -1   0  1
+#   0  -1  1
