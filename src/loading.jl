abf35c924c4bcc6ba67a2f7c47bf4f0330736c40
scott <scott.stanie@gmail.com>
2019-06-27 22:52:14 -0500

    get load stack working with multiple dispatch

--- src/loading.jl
+++ src/loading.jl
@@ -37,14 +37,18 @@ function load(filename::String; rsc_file::Union{String, Nothing}=nothing)
     # Sentinel files should have .rsc file: check for dem.rsc, or elevation.rsc
     rsc_data = nothing
 	if !isnothing(rsc_file)
-		rsc_data = sario.load(rsc_file)::Dict{String, Any}
+		rsc_data = sario.load(rsc_file)
 	end
 
     if ext in SENTINEL_EXTS
 		if isnothing(rsc_file)
 			rsc_file = sario.find_rsc_file(filename)
 		end
-        rsc_data = sario.load(rsc_file)
+		rsc_data = sario.load(rsc_file)
+	end
+
+	if !isnothing(rsc_data)
+		rsc_data = convert(Dict{String, Any}, rsc_data)
 	end
 
     if ext in STACKED_FILES
@@ -52,7 +56,7 @@ function load(filename::String; rsc_file::Union{String, Nothing}=nothing)
 	end
     # having rsc_data implies that this is not a UAVSAR file, so is complex
 	# TODO: haven"t transferred over UAVSAR functions, so no load_real yet
-	return load_complex(filename, rsc_data=rsc_data)
+	load_complex(filename, rsc_data)
 
 end
 
@@ -62,15 +66,116 @@ end
 """Extracts the file extension, including the "." (e.g.: .slc)"""
 get_file_ext(filename::String) = splitext(filename)[end]
 
+function load_complex(filename::String, rsc_data::Dict{String, Any})
+	rows = rsc_data["file_length"]
+	cols = rsc_data["width"]
+	# Note: must be saved in c/row-major order, so loading needs a transpose
+	out = Array{ComplexF32, 2}(undef, (cols, rows))
 
-function read_stack(file_ext::String, rows::Int, cols::Int)
-	allfiles = Glob.glob(file_ext, ".")
-	buffer = Array{Float32, 2}(undef, (rows, cols))
-    stack = Array{Float32, 3}(undef, (rows, cols, length(allfiles)))
-    for (idx, f) in enumerate(allfiles)
+	read!(filename, out)
+	return permutedims(out)
+end
+
+
+"""load_stacked_img is for weirdly formatted images:
+
+Format is two stacked matrices:
+	[[first], [second]] where the first "cols" number of floats
+	are the first matrix, next "cols" are second, etc.
+For .unw height files, the first is amplitude, second is phase (unwrapped)
+For .cc correlation files, first is amp, second is correlation (0 to 1)
+"""
+function load_stacked_img(filename::String, rsc_data::Dict{String, Any})
+	rows = rsc_data["file_length"]
+	cols = rsc_data["width"]
+	# Note: must be saved in c/row-major order, so loading needs a transpose
+	# out_left usually is amplitude data
+	# Usually we are interested in out_right
+	#
+	# First make container for all of data
+	out = Array{Float32, 2}(undef, (2cols, rows))
+	read!(filename, out)
+
+	# TODO: port over rest of code for handling amp (if we care about that)
+	# out_left = out[1:cols, :]
+	return permutedims(out[cols+1:end, :])
+end
+
+
+# TODO: probably a better way to do this.. but can't figure out how to
+# without allocating so many arrays that it's slow as Python
+function load_stack(; file_list::Union{Array{String}, Nothing}=nothing, 
+					directory::Union{String, Nothing}=nothing,
+					file_ext::Union{String, Nothing}=nothing)
+	if isnothing(file_list)
+		file_list = Glob.glob("*$file_ext", directory)
+	end
+
+
+	# rsc_data = sario.load(sario.find_rsc_file(basepath=directory))
+	test_arr = load(file_list[1])
+	rows, cols = size(test_arr)
+	T = eltype(test_arr)
+
+	stack, buffer = _return_array(T, rows, cols, length(file_list))
+    for (idx, f) in enumerate(file_list)
  		read!(f, buffer)
  	   	stack[:, :, idx] = buffer
     end
-    return stack
+
+	return _permute(stack)
+	# throw(ArgumentError(file_ext, "cant make stack from $file_ext"))
+	# end
 end
 
+# Using multiple dispatch to avoid if statements for 2 types of images
+function _return_array(T::Type{ComplexF32}, rows::Int, cols::Int, file_len::Int)
+	stack = Array{T, 3}(undef, (cols, rows, file_len))
+	buffer = Array{T, 2}(undef, (cols, rows))
+	stack, buffer
+end
+
+function _return_array(T::Type{Float32}, rows::Int, cols::Int, file_len::Int)
+	stack = Array{T, 3}(undef, (2cols, rows, file_len))
+	buffer = Array{T, 2}(undef, (2cols, rows))
+	stack, buffer
+end
+
+function _permute(stack::Array{ComplexF32, 3})
+	return permutedims(stack[cols+1:end, :, :], (2, 1, 3))
+end
+
+function _permute(stack::Array{Float32, 3})
+	return permutedims(stack, (2, 1, 3))
+end
+
+
+
+# function _load_stack_complex(file_list::Array{String}, rows::Int, cols::Int)
+# 	stack = Array{ComplexF32, 3}(undef, (cols, rows, length(file_list)))
+# 	buffer = Array{ComplexF32, 2}(undef, (cols, rows))
+# 
+#     for (idx, f) in enumerate(file_list)
+#  		read!(f, buffer)
+#  	   	stack[:, :, idx] = buffer
+#     end
+# 
+# 	return permutedims(stack, (2, 1, 3))
+# end
+# 
+# 
+# function _load_stack_stacked(file_list::Array{String}, rows::Int, cols::Int)
+# 	stack = Array{Float32, 3}(undef, (2cols, rows, length(file_list)))
+# 	buffer = Array{Float32, 2}(undef, (2cols, rows))
+# 
+#     for (idx, f) in enumerate(file_list)
+#  		read!(f, buffer)
+#  	   	stack[:, :, idx] = buffer
+#     end
+# 	return permutedims(stack[cols+1:end, :, :], (2, 1, 3))
+# end
+
+
+# function save(filename::String, arr::Array{T, } ; kwargs...)
+# sario.save(filename, arr, kwargs...)
+# end
