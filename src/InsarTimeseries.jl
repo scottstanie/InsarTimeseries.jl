87726d2bd9e81ea44a5e5965356d362b56d6c6ef
scott <scott.stanie@gmail.com>
2019-06-29 12:56:51 -0500

    move pycall to main file

--- src/InsarTimeseries.jl
+++ src/InsarTimeseries.jl
@@ -6,15 +6,23 @@ module InsarTimeseries
 
 using Dates
 
+# Include sario here so it is available to all
+using PyCall
+const sario = PyNULL()
+
+function __init__()
+	copy!(sario, pyimport("apertools.sario"))
+end
+
 include("./loading.jl")
 include("./timeseries.jl")
 
 
 export load,
+	run_inversion,
 	# load_file, 
-	read_stack, 
-	get_file_ext,
-	matrixA
+	load_stack, 
+	get_file_ext
 
 end # module
 
