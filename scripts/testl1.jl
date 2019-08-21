af61e5d025f59f10517806bbeb44c9501d4cdabc
scott <scott.stanie@gmail.com>
2019-08-20 20:18:24 -0500

    functions testing means/solutions

--- scripts/testl1.jl
+++ scripts/testl1.jl
@@ -6,6 +6,7 @@ using Dates
 using ECOS
 using HDF5
 using LinearAlgebra
+using Statistics: mean
 using Plots
 using PyCall
 import Polynomials
@@ -15,8 +16,8 @@ import Polynomials
 # #df = create_insar_gps_df(geo_path, defo_filename=defo_filename, reference_station=reference_station)
 
 YLIMS = (-12, 12)
-const station_name_list = ["NMHB", "TXAD", "TXBG", "TXBL", "TXCE", "TXFS", "TXKM", "TXL2",
-                           "TXMC", "TXMH", "TXOE", "TXOZ", "TXS3", "TXSO"]
+station_name_list = ["NMHB", "TXAD", "TXBG", "TXBL", "TXCE", "TXFS", "TXKM", 
+                     "TXL2", "TXMC", "TXMH", "TXOE", "TXOZ", "TXS3", "TXSO"]
 
 latlon = pyimport("apertools.latlon")
 sario = pyimport("apertools.sario")
@@ -106,19 +107,22 @@ function solve_insar_ts(unw_vals::Array{<:AbstractFloat, 1})
     v_linear_lstsq = Blin \ unw_vals
     v_unreg_lstsq = B \ unw_vals
 
-    # solver = SCSSolver()
-    solver = ECOSSolver(verbose=0)
+    # # solver = SCSSolver()
+    # solver = ECOSSolver(verbose=0)
+    # prob_linear = minimize(norm(Blin*v_linear_l1 - unw_vals, 1))
+    # prob_unreg = minimize(norm(B*v_unreg_l1 - unw_vals, 1))
+    # solve!(prob_linear, solver)
+    # solve!(prob_unreg, solver)
+    # # Using functions in module:
+    var_linear = Variable(1)
+    v_linear_l1 = InsarTimeseries.invert_pixel(unw_vals, Blin, var_linear)
+    var_unreg = Variable(size(B, 2))
+    v_unreg_l1 = InsarTimeseries.invert_pixel(unw_vals, B, var_unreg)
 
-    v_linear_l1 = Variable(1)
-    prob_linear = minimize(norm(Blin*v_linear_l1 - unw_vals, 1))
+    # v_linear_l1 = InsarTimeseries.invert_pixel(unw_vals, Blin, rho=1.0, alpha=1.5)
+    # v_unreg_l1 = InsarTimeseries.invert_pixel(unw_vals, Blin, rho=1.0, alpha=1.5)
 
-    v_unreg_l1 = Variable(size(B, 2))
-    prob_unreg = minimize(norm(B*v_unreg_l1 - unw_vals, 1))
-
-
-    solve!(prob_linear, solver)
-    solve!(prob_unreg, solver)
-    return v_linear_lstsq, v_unreg_lstsq, Float32.([v_linear_l1.value]), Float32.(reshape(v_unreg_l1.value, :))
+    return v_linear_lstsq, v_unreg_lstsq, v_linear_l1, v_unreg_l1
 end
 
 function integrate_velos(v_linear_lstsq, v_unreg_lstsq, v_linear_l1, v_unreg_l1)
@@ -129,8 +133,9 @@ function integrate_velos(v_linear_lstsq, v_unreg_lstsq, v_linear_l1, v_unreg_l1)
     return linear_lstsq, unreg_lstsq, linear_l1, unreg_l1
 end
 
-function plot_insar(geolist, insar_linear_ts, insar_unreg_ts; title="")
-    p = plot(geolist, insar_linear_ts, title=title, label="linear insar", ylabel="cm", linewidth=3, ylims=YLIMS, legend=:bottomleft)
+function plot_insar(geolist, insar_linear_ts, insar_unreg_ts; title="", ylims=YLIMS)
+    p = plot(geolist, insar_linear_ts, title=title, label="linear insar", ylabel="cm", 
+             linewidth=3, ylims=ylims, legend=:bottomleft)
     plot!(p, geolist, insar_unreg_ts, label="unreg insar", marker=:o, linealpha=0.0)
     return p
 end
@@ -196,24 +201,26 @@ end
 
 l1_errors, l2_errors = [], []
 
-# station_name = station_name_list[1]
-# station_name = "TXOZ"
 plotting = false
 
+# station_name = station_name_list[1]
+# station_name = "TXOZ"
+# station_name_list = ["TXOZ"]
 
 # l1_error, l2_error = process_pixel(station_name, UNW_STACK_FILE, GEOLIST, INTLIST, VALID_IGRAM_INDICES, plotting=plotting)
 # append!(l2_errors, l2_error)
 # append!(l1_errors, l1_error)
-for station_name in station_name_list
-    l1_error, l2_error = process_pixel(station_name=station_name, plotting=plotting)
-    append!(l2_errors, l2_error)
-    append!(l1_errors, l1_error)
-end
-println("TOTAL ERRORS (all in mm / year of velocity):")
-println("L2 RMS error for all stations: $(rms(l2_errors))")
-println("L1 RMS error for all stations: $(rms(l1_errors))")
-println("L2 sum of abs errors for all stations: $(total_abs_error(l2_errors))")
-println("L1 sum of abs errors for all stations: $(total_abs_error(l1_errors))")
+#
+# for station_name in station_name_list
+#     l1_error, l2_error = process_pixel(station_name=station_name, plotting=plotting)
+#     append!(l2_errors, l2_error)
+#     append!(l1_errors, l1_error)
+# end
+# println("TOTAL ERRORS (all in mm / year of velocity):")
+# println("L2 RMS error for all stations: $(rms(l2_errors))")
+# println("L1 RMS error for all stations: $(rms(l1_errors))")
+# println("L2 sum of abs errors for all stations: $(total_abs_error(l2_errors))")
+# println("L1 sum of abs errors for all stations: $(total_abs_error(l1_errors))")
 # end
 
 # main()
@@ -232,3 +239,55 @@ println("L1 sum of abs errors for all stations: $(total_abs_error(l1_errors))")
 # ts_unreg = h5read(DEFO_FILENAME, "stack", (col, row, :))[1, 1, :];
 # p1 = plot(GEOLIST, ts_linear)
 # plot!(p1, GEOLIST, ts_unreg)
+
+
+unw_vals_by_date(date, intlist, unw_vals) = unw_vals[date in intlist]
+Blins_by_date(date, intlist, Blin) = Blin[date in intlist, :]
+
+function remove_date(bad_date, intlist, unw_vals, B)
+    good_idxs = .!(bad_date in intlist)
+    B_clean = B[good_idxs, :]
+    unw_clean = unw_vals[good_idxs]
+    return B_clean, unw_clean
+end
+
+function solve_without_date(bad_date, intlist, unw_vals, B)
+    B_clean, unw_clean = remove_date(bad_date, intlist, unw_vals, B)
+    velo_l1 = InsarTimeseries.invert_pixel(unw_clean, B_clean, rho=1.0, alpha=1.5)
+    velo_lstsq = B_clean \ unw_clean
+    # Return soluyion in mm/year
+    return PHASE_TO_CM * 10 * 365 .* (velo_l1[1], velo_lstsq[1])
+end
+
+function compare_solutions(geolist, intlist, unw_vals, B)
+    base_l1, base_lstsq = solve_without_date(Date(2000,1,1), intlist, unw_vals, B)
+    l1_diffs = Array{Float32, 1}(undef, length(geolist))
+    lstsq_diffs = similar(l1_diffs)
+    for (idx, d) in enumerate(geolist)
+        l1, lstsq = solve_without_date(d, intlist, unw_vals, B)
+        l1_diffs[idx] =  l1 - base_l1
+        lstsq_diffs[idx] = lstsq - base_lstsq
+    end
+    return l1_diffs, lstsq_diffs
+end
+
+mean_abs_val(geolist, intlist, unw_vals) = [mean(abs.(unw_vals_by_date(d, intlist, unw_vals)))
+                                             for d in geolist];
+
+function geos_with_good_mean(geolist, intlist, unw_vals; cutoff=12)
+    means_by_date = mean_abs_val(geolist, intlist, unw_vals)
+    good_geo_idxs = [idx for (idx, val) in enumerate(means_by_date) if val < cutoff];
+    bad_geo_idxs = [idx for (idx, val) in enumerate(means_by_date) if val >= cutoff];
+
+    good_geos = geolist[good_geo_idxs]
+    bad_geos = geolist[bad_geo_idxs]
+
+    # good_igrams = filter(igram -> any(g in igram for g in good_geos), intlist)
+    good_igrams = filter(igram -> !(igram in bad_geos), intlist)
+
+    good_unw_idxs = indexin(good_igrams, intlist)
+    good_unw_vals = unw_vals[good_unw_idxs]
+
+    good_geos, good_igrams, good_unw_vals
+end
+
