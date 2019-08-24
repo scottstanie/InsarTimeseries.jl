7aa325e641bd440353c458de23725b164e67f67f
scott <scott.stanie@gmail.com>
2019-08-24 13:37:35 -0500

    working masking of geos

--- src/prepare.jl
+++ src/prepare.jl
@@ -1,66 +1,105 @@
-using Distributed
+using Distributed: pmap, myid, workers, WorkerPool
+
+find_files(ext, directory=".") = sort(Glob.glob("*"*ext, directory))
 
 """Runs a reference point shift on flattened stack of unw files stored in .h5"""
-function deramp_unw_file(unw_stack_file::String; overwrite=false, stack_flat_dset=nothing)
-    if isnothing(stack_flat_dset)
-        stack_flat_dset = STACK_FLAT_DSET
-    end
-    println("Writing deramped unws to $stack_flat_dset")
+function deramp_unws(directory="."; input_ext=".unw", output_ext=".unwflat", overwrite=true)
+    println("Writing deramped unws to $output_ext files")
+    in_files = find_files(input_ext, directory)
+    out_files = [replace(fname, input_ext => output_ext) for fname in in_files]
 
-    # If we already have this dataset made, skip the function
-    if !sario.check_dset(unw_stack_file, stack_flat_dset, overwrite)
+    # If we already have these files made, skip the function
+    if all(isfile(f) for f in out_files) && !overwrite
+        println("Output Files, overwrite = $overwrite. Skipping")
         return nothing
     end
 
     println("Starting stack deramp ")
-    h5open(unw_stack_file, "cw") do f
-        if !(STACK_DSET in names(f))
-            throw("Need $STACK_DSET to be created in $unw_stack_file before deramp stack can be run")
-        end
-
-        stack_in = f[STACK_DSET]
-
-        d_create(f,
-            stack_flat_dset,
-            datatype(Float32),
-            dataspace(size(stack_in)),
-        )
-        @show stack_in
-        @show f[stack_flat_dset]
-    end
-    f = h5open(unw_stack_file)
-    nrows, ncols, nlayers = size(f[STACK_DSET])
-    close(f)
-
-    # fmask = h5open("masks.h5", "r")
-
-    chunk_size = 100
-    chunk = Array{Float32, 3}(undef, (nrows, ncols, chunk_size))
-    chunk_out = similar(chunk)
-    mask_chunk = Array{Bool, 3}(undef, (nrows, ncols, chunk_size))
-    k = 1
-    while k < nlayers
-        kend = min(k + chunk_size - 1, nlayers)
-        numch = kend - k + 1
-        println("reading $k : $kend")
-        @time chunk[:, :, 1:numch] .= h5read(unw_stack_file, STACK_DSET, (:, :, k:kend))
-        mask_chunk[:, :, 1:numch] .= h5read("masks.h5", IGRAM_MASK_DSET, (:, :, k:kend))
-        println("calculating ramp")
-        @time @inbounds Threads.@threads for jdx = 1:numch
-            chunk_out[:, :, jdx] .= remove_ramp(view(chunk, :, :, jdx), view(mask_chunk, :, :, jdx))
-        end
-
-        println("writing chunk layers")
-        @time h5open(unw_stack_file, "cw") do f
-            for jdx = 1:numch
-                f[stack_flat_dset][:, :, k+jdx-1] = view(chunk_out,:, :, jdx)
-            end
-        end
-
-        k += chunk_size
-    end
-
+    loop_over_files((arr, mask) -> remove_ramp(arr, mask), in_files, out_files)
     println("Deramping stack complete")
+end
+
+function _create_dset(h5file, dset_name, shape, dtype)
+    # TODO: add a chunking option for ones we access depth-wise vs layer-wise
+    # dset = d_create(g, "B", datatype(Float64), dataspace(1000,100,10), "chunk", (100,100,1))
+    h5open(h5file, "cw") do f
+        d_create(f,
+            dset_name,
+            datatype(dtype),
+            dataspace(shape),
+        )
+    end
+end
+
+# Note: 6 workers for the .geo.mask file creation seems fastest
+# More and they thrash while reading
+function create_mask_stacks(igram_path, mask_filename=nothing, geo_path=nothing, overwrite=false)
+    if isnothing(mask_filename)
+        mask_file = joinpath(igram_path, MASK_FILENAME)
+    end
+    if isnothing(geo_path)
+        geo_path = utils.get_parent_dir(igram_path)
+    end
+
+    row_looks, col_looks = utils.find_looks_taken(igram_path, geo_path=geo_path)
+    dem_rsc = load(sario.find_rsc_file(directory=igram_path))
+    # TODO: make sure the masks geo/intlists are togeher...
+    # Maybe just lump them with the unw file
+    # sario.save_dem_to_h5(mask_file, dem_rsc, dset_name=DEM_RSC_DSET, overwrite=overwrite)
+    # save_geolist_to_h5(igram_path, mask_file, overwrite=overwrite)
+    # save_intlist_to_h5(igram_path, mask_file, overwrite=overwrite)
+
+    # TODO: do the overwrite check
+    loop_over_files(_get_geo_mask, geo_path, ".geo", ".geo.mask",
+                    looks=(row_looks, col_looks))
+
+    compute_int_masks(
+        mask_file=mask_file,
+        igram_path=igram_path,
+        geo_path=geo_path,
+        row_looks=row_looks,
+        col_looks=col_looks,
+        dem_rsc=dem_rsc,
+        overwrite=overwrite,
+    )
+end
+
+_get_geo_mask(arr) = (arr .== 0)
+
+"""Creates igram masks by taking the logical-or of the two .geo files
+
+Assumes save_geo_masks already run
+"""
+function compute_int_masks(
+        igram_path=None,
+        geo_path=None,
+        dem_rsc=None,
+        dset_name=IGRAM_MASK_DSET,
+        overwrite=False,
+)
+    # TODO: add checks for overwrite
+
+    int_date_list = sario.find_igrams(directory=igram_path)
+    int_file_list = sario.find_igrams(directory=igram_path, parse=false)
+
+    geo_date_list = sario.find_geos(directory=geo_path)
+
+
+    geo_mask_stack = f[GEO_MASK_DSET]
+    int_mask_dset = f[dset_name]
+    # TODO: fix/pass to parallel
+    for (idx, (early, late)) in enumerate(int_date_list)
+        early_idx = geo_date_list.index(early)
+        late_idx = geo_date_list.index(late)
+        early_mask = geo_mask_stack[early_idx]
+        late_mask = geo_mask_stack[late_idx]
+
+        new_mask = early_mask .| late_mask
+        int# _mask_sum .+= new_mask
+
+    end
+    # # Also create one image of the total masks
+    # f[IGRAM_MASK_SUM_DSET] = sum(int_mask_dset, dims=0)
 end
 
 
@@ -229,22 +268,47 @@ function create_mean_hdf5(h5file::String; dset_name::String=STACK_DSET)
     end
 end
 
-function loop_over_files(f, directory, input_ext, output_ext)
-    file_list = Glob.glob("*"*input_ext, directory)
-    println("Looping over files:")
-    @show file_list
 
-    # Threads.@threads for fname in file_list
-    # @sync @distributed for fname in file_list
-    # pmap(file_list, _load_and_run(f)
-    pmap(fname -> _load_and_run(f, fname, input_ext, output_ext), file_list)
+"""Parallel way to run over many image files
+Pass a function `f` to operate on each image 
+
+    With 8 workers, on 100 files of 1000x1000
+
+    @time InsarTimeseries.loop_over_files(add1, ".", ".int", "_flat.int")
+
+    runs in 1 second, vs about 8 seconds serially
+"""
+function loop_over_files(f, directory::String, input_ext::String, output_ext::String;
+                         looks=(1, 1), max_procs::Union{Nothing, Int}=nothing)
+    in_files = find_files(input_ext, directory)
+    println("Looping over files: $in_files")
+    out_files = [replace(fname, input_ext => output_ext) for fname in in_files]
+
+    wp = _get_workerpool(max_procs)
+    pmap((name_in, name_out) -> _load_and_run(f, name_in, name_out, looks=looks),
+         wp, in_files, out_files)
+    # @sync @distributed for fname in in_files
     # end
 end
-function _load_and_run(f, fname, input_ext, output_ext)
-    # println("On thread id $(Threads.threadid()): Input: $fname, out: $outfile")
-    outfile = replace(fname, input_ext => output_ext)
-    println("On proc id $(Distributed.myid()): Input: $fname, out: $outfile")
-    input_arr = load(fname)
+
+"""To manually pass which files you want to run on and save to""" 
+function loop_over_files(f, in_files::Array{String, 1}, out_files::Array{String, 1};
+                         looks=(1, 1), max_procs::Union{Nothing, Int}=nothing)
+    wp = _get_workerpool(max_procs)
+    pmap((name_in, name_out) -> _load_and_run(f, name_in, name_out, looks=looks),
+         wp, in_files, out_files)
+end
+
+function _load_and_run(f, name_in, name_out; looks=(1, 1))
+    println("On proc id $(myid()): Input: $name_in, out: $name_out")
+    input_arr = load(name_in, looks=looks)
     out_arr = f(input_arr)
-    save(outfile, out_arr)
+    save(name_out, out_arr)
+end
+function _get_workerpool(max_procs)
+    if isnothing(max_procs)
+        return WorkerPool(workers())
+    else
+        return WorkerPool(workers()[1:max_procs])
+    end
 end
