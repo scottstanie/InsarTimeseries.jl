eac6287487725615139c045fb2d9a4a9c4f681f9
scott <scott.stanie@gmail.com>
2019-08-27 16:15:49 -0500

    working reference recalculation, full error matrix

--- scripts/testl1.jl
+++ scripts/testl1.jl
@@ -1,15 +1,17 @@
 push!(LOAD_PATH,"/home/scott/repos/InsarTimeseries.jl/src/")
 # import InsarTimeseries
 import InsarTimeseries: PHASE_TO_CM, STACK_FLAT_SHIFTED_DSET, STACK_FLAT_DSET
-import Convex
 using Dates
-import ECOS
 using HDF5
 using LinearAlgebra
 using Statistics: mean
 using Plots
 using PyCall
 import Polynomials
+import NaNMath
+nm = NaNMath
+# import Convex
+# import ECOS
 # import SCS
 
 # TODO: USE REFERENCE STATION AND this
@@ -18,6 +20,10 @@ import Polynomials
 YLIMS = (-12, 12)
 station_name_list = ["NMHB", "TXAD", "TXBG", "TXBL", "TXCE", "TXFS", "TXKM", 
                      "TXL2", "TXMC", "TXMH", "TXOE", "TXOZ", "TXS3", "TXSO"]
+
+# REFERENCE_STATION = "TXKM"
+# REFERENCE_STATION = "TXAD"
+REFERENCE_STATION = nothing
 
 latlon = pyimport("apertools.latlon")
 sario = pyimport("apertools.sario")
@@ -44,62 +50,58 @@ row, col = 1838, 1367
 row, col = 1223, 943
 
 
-# Functions for error evaluation
-rms(arr) = sqrt(mean(arr.^2))
-total_abs_error(arr) = sum(abs.(arr))
-
-
-function get_unw_vals(unw_stack_file::String, station_name::String, window=5, dset=STACK_FLAT_SHIFTED_DSET)
+function get_unw_vals(unw_stack_file::String, station_name::String, window=5,
+                      dset=STACK_FLAT_DSET, valid_indices=VALID_IGRAM_INDICES;
+                      reference_station=nothing)
     dem_rsc = sario.load("dem.rsc")
     lon, lat = gps.station_lonlat(station_name)
     row, col = map(x-> convert(Int, x), 
                    latlon.nearest_pixel(dem_rsc, lon=lon, lat=lat))
 
-    println("Getting data at $station_name: row $row, col $col, lon $lon, lat $lat")
-    @time unw_vals = _read_unw(unw_stack_file, row, col, window, dset)
-    return unw_vals
+    # println("Getting data at $station_name: row $row, col $col, lon $lon, lat $lat")
+    unw_vals = _read_unw(unw_stack_file, row, col, window, dset, valid_indices)
+    return _subtract_reference(unw_vals, reference_station, unw_stack_file, window, dset, valid_indices)
 end
 
-function get_unw_vals(unw_stack_file::String, row::Int, col::Int, window=5, dset=STACK_FLAT_SHIFTED_DSET)
-    @time unw_vals = _read_unw(unw_stack_file, row, col, window, dset)
-    return unw_vals
+function get_unw_vals(unw_stack_file::String, row::Int, col::Int, window=5,
+                      dset=STACK_FLAT_DSET, valid_indices=VALID_IGRAM_INDICES;
+                      reference_station=nothing)
+    unw_vals = _read_unw(unw_stack_file, row, col, window, dset, valid_indices)
+    return _subtract_reference(unw_vals, reference_station, unw_stack_file, window, dset, valid_indices)
 end
 
-function _read_unw(unw_stack_file, row, col, window=5, dset=STACK_FLAT_SHIFTED_DSET)
-    println("Loading $row, $col from $dset")
+function _read_unw(unw_stack_file, row, col, window=5, dset=STACK_FLAT_DSET, valid_indices=VALID_IGRAM_INDICES)
+    # println("Loading $row, $col from $dset, avging window $window")
     halfwin = div(window, 2)
     # Note: swapping the col and row in h5read since julia is col-major
     unw_depth = h5read(unw_stack_file, dset, (col-halfwin:col+halfwin, row-halfwin:row+halfwin, :))
     unw_vals_all = vec(mean(unw_depth, dims=(1,2)))
-    unw_vals = unw_vals_all[VALID_IGRAM_INDICES]
+    unw_vals = unw_vals_all[valid_indices]
+end
+
+function _subtract_reference(unw_vals, reference_station, unw_stack_file, window, dset, valid_indices)
+    isnothing(reference_station) && return unw_vals
+    return unw_vals - get_unw_vals(unw_stack_file, reference_station, window, dset, valid_indices)
 end
 
 function get_gps_los(station_name, geo_path="../"; reference_station=nothing)
-    dts, gps_los_data = gps.load_gps_los_data(geo_path, station_name, start_year=2015, end_year=2018, zero_mean=true)
-    dts = convert(Array{Dates.Date, 1}, dts)
-    return dts, gps_los_data
+    dts, gps_los_data = gps.load_gps_los_data(geo_path, station_name, start_year=2015, end_year=2018,
+                                              zero_mean=true, reference_station=reference_station)
+    
+    return [convert(Date, d) for d in dts], gps_los_data
 end
 
-_get_day_nums(dts) = [( d - dts[1]).value for d in dts]
-
-function fit_line(dts, data)
-    day_nums = _get_day_nums(dts)
-    p = Polynomials.polyfit(day_nums, data, 1)
-    # p(day_nums[end])
-    return p
+function solve_insar_ts(station_name::String, window::Int=5, reference_station=nothing)
+    unw_vals = get_unw_vals(UNW_STACK_FILE, station_name, window, reference_station=reference_station)
+    return solve_insar_ts(unw_vals, window, reference_station)
 end
 
-function solve_insar_ts(station_name::String, window=5)
-    unw_vals = get_unw_vals(UNW_STACK_FILE, station_name, window)
-    return solve_insar_ts(unw_vals)
+function solve_insar_ts(row::Int, col::Int, window::Int=5, reference_station=nothing)
+    unw_vals = get_unw_vals(UNW_STACK_FILE, row, col, window, reference_station=reference_station)
+    return solve_insar_ts(unw_vals, window, reference_station)
 end
 
-function solve_insar_ts(row::Int, col::Int, window=5)
-    unw_vals = get_unw_vals(UNW_STACK_FILE, row, col, window)
-    return solve_insar_ts(unw_vals)
-end
-
-function solve_insar_ts(unw_vals::Array{<:AbstractFloat, 1})
+function solve_insar_ts(unw_vals::Array{<:AbstractFloat, 1}, window::Int=5)
 
     B = InsarTimeseries.build_B_matrix(GEOLIST, INTLIST)
     Blin = sum(B, dims=2)
@@ -143,19 +145,27 @@ function plot_gps!(p, dts, gps_los_data, gps_poly)
     plot!(p, dts, gps_poly(day_nums), color=:green, linewidth=3, label="gps line fit")
 end
 
+_get_day_nums(dts) = [( d - dts[1]).value for d in dts]
 
-function process_pixel(; station_name=nothing, plotting=false, reference_station=nothing, window=5)
-    println("Processing $station_name")
+function fit_line(dts, data)
+    day_nums = _get_day_nums(dts)
+    p = Polynomials.polyfit(day_nums, data, 1)
+    # p(day_nums[end])
+    return p
+end
+
+function process_pixel(; station_name=nothing, plotting=false, reference_station=REFERENCE_STATION,
+                       window=5, verbose=true)
     total_interval_days = (GEOLIST[end] - GEOLIST[1]).value
 
     # First solve for the velocities in L1 vs L2 to compare to GPS
-    v_linear_lstsq, v_unreg_lstsq, v_linear_l1, v_unreg_l1 = solve_insar_ts(station_name, window)
+    v_linear_lstsq, v_unreg_lstsq, v_linear_l1, v_unreg_l1 = solve_insar_ts(station_name, window, reference_station)
 
-    # dts, gps_los_data = get_gps_los(station_name, reference_station=REFERENCE_STATION)
-    dts, gps_los_data = get_gps_los(station_name)
+    dts, gps_los_data = get_gps_los(station_name, reference_station=reference_station)
     # Convert to "days since start" for line fitting
     gps_poly = fit_line(dts, gps_los_data)
-    offset, slope = Polynomials.coeffs(gps_poly)
+    slope = length(gps_poly) == 2 ? Polynomials.coeffs(gps_poly)[2] : Polynomials.coeffs(gps_poly)[1]
+    # offset, slope = Polynomials.coeffs(gps_poly)
     slope_gps_mm_yr = 365 * 10 * slope
 
     slope_insar_l2_mm_yr = (365 * 10 * PHASE_TO_CM * v_linear_lstsq)[1] # solution currently in phase
@@ -165,13 +175,16 @@ function process_pixel(; station_name=nothing, plotting=false, reference_station
     l1_error = slope_gps_mm_yr - slope_insar_l1_mm_yr
 
 
-    println("GPS velocity (mm / year): $slope_gps_mm_yr")
-    println("InSAR L2 linear velocity (mm / year): $slope_insar_l2_mm_yr")
-    println("InSAR L1 linear velocity (mm / year): $slope_insar_l1_mm_yr")
-    println("==="^10)
-    println("Difference (mm / yr): GPS - Insar L2 solution: $l2_error")
-    println("Difference: (mm / yr) GPS - Insar L1 solution: $l1_error")
-    println("==="^10)
+    if verbose
+        @show station_name reference_station
+        println("GPS velocity (mm / year): $slope_gps_mm_yr")
+        println("InSAR L2 linear velocity (mm / year): $slope_insar_l2_mm_yr")
+        println("InSAR L1 linear velocity (mm / year): $slope_insar_l1_mm_yr")
+        println("==="^10)
+        println("Difference (mm / yr): GPS - Insar L2 solution: $l2_error")
+        println("Difference: (mm / yr) GPS - Insar L1 solution: $l1_error")
+        println("==="^10)
+    end
 
     # Now get the full time series of insar by integrating
     linear_lstsq, unreg_lstsq, linear_l1, unreg_l1 = integrate_velos(v_linear_lstsq,
@@ -194,6 +207,26 @@ function process_pixel(; station_name=nothing, plotting=false, reference_station
     return l1_error, l2_error
 end
 
+function calc_error_matrix(station_name_list, plotting=false)
+    println("Calculating errors at all station/reference combos")
+    l1_error_matrix = zeros(length(station_name_list), length(station_name_list))
+    l2_error_matrix = zeros(length(station_name_list), length(station_name_list))
+
+    for (i, ref_name) in enumerate(station_name_list)
+        for (j, station_name) in enumerate(station_name_list)
+            l1_error, l2_error = process_pixel(station_name=station_name, plotting=plotting, 
+                                               reference_station=ref_name, verbose=false)
+            l1_error_matrix[i, j] = l1_error
+            l2_error_matrix[i, j] = l2_error
+        end
+    end
+    return l1_error_matrix, l2_error_matrix
+end
+
+# Functions for error evaluation
+rms(arr) = sqrt(nm.mean(arr.^2))
+total_abs_error(arr) = nm.sum(abs.(arr))
+
 
 l1_errors, l2_errors = [], []
 
@@ -201,25 +234,27 @@ plotting = false
 
 # station_name = station_name_list[1]
 # station_name = "TXOZ"
-station_name_list = ["TXMH"]
+# station_name_list = ["TXMH"]
 
 # l1_error, l2_error = process_pixel(station_name, UNW_STACK_FILE, GEOLIST, INTLIST, VALID_IGRAM_INDICES, plotting=plotting)
 # append!(l2_errors, l2_error)
 # append!(l1_errors, l1_error)
-#
-for station_name in station_name_list
-    l1_error, l2_error = process_pixel(station_name=station_name, plotting=plotting)
-    append!(l2_errors, l2_error)
-    append!(l1_errors, l1_error)
-end
-println("TOTAL ERRORS (all in mm / year of velocity):")
-println("L2 RMS error for all stations: $(rms(l2_errors))")
-println("L1 RMS error for all stations: $(rms(l1_errors))")
-println("L2 sum of abs errors for all stations: $(total_abs_error(l2_errors))")
-println("L1 sum of abs errors for all stations: $(total_abs_error(l1_errors))")
-println("L2 maximum errors : $(maximum(l2_errors))")
-println("L1 maximum errors : $(maximum(l1_errors))")
 
+# for station_name in station_name_list
+#     l1_error, l2_error = process_pixel(station_name=station_name, plotting=plotting)
+#     append!(l2_errors, l2_error)
+#     append!(l1_errors, l1_error)
+# end
+
+# println("TOTAL ERRORS (all in mm / year of velocity):")
+# println("L2 RMS error for all stations: $(rms(l2_errors))")
+# println("L1 RMS error for all stations: $(rms(l1_errors))")
+# println("L2 sum of abs errors for all stations: $(total_abs_error(l2_errors))")
+# println("L1 sum of abs errors for all stations: $(total_abs_error(l1_errors))")
+# println("L2 maximum errors : $(maximum(l2_errors))")
+# println("L1 maximum errors : $(maximum(l1_errors))")
+
+@time l1_error_matrix, l2_error_matrix = calc_error_matrix(station_name_list, plotting)
 
 
 # function check_convex_l2(Blin, unw_vals)
@@ -246,3 +281,5 @@ function plotunws(B1, B2, unw1, unw2)
     plt.show()
     return fig, axes
 end
+
+# function shift_and_compare(ref_station)
