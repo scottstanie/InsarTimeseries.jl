bcbef70cfbbd84e7f1a7c4a13d55c11d31a57294
scott <scott.stanie@gmail.com>
2019-09-03 21:21:20 -0500

    add sbas A and B tests

--- test/test_sbas.jl
+++ test/test_sbas.jl
@@ -1,10 +1,16 @@
 using Dates
 
+# Test data is 3 days after a starting reference on Jan 1:
+# The actual phi phase is [0, 1, 2, 3]
+ 
 geolist = [Date(2019,1,1), Date(2019,1,3), Date(2019,1,6), Date(2019,1,10)]
+timediffs = InsarTimeseries.day_diffs(geolist)
+@test timediffs == [2, 3, 4]
+
 intlist = [(geolist[i], geolist[j]) for i in 1:length(geolist)-1 for j in i+1:length(geolist)]
 
 # julia> intlist
-#  (2019-01-01, 2019-01-03)
+#  (2019-01-01, 2019-01-03)  
 #  (2019-01-01, 2019-01-06)
 #  (2019-01-01, 2019-01-10)
 #  (2019-01-03, 2019-01-06)
@@ -26,5 +32,21 @@ B = InsarTimeseries.build_B_matrix(geolist, intlist)
            0.0  3.0  0.0;
            0.0  3.0  4.0;
            0.0  0.0  4.0]
+
+truth_phi = [0.0, 1.0, 2.0, 3.0]
+truth_v = [1/2, 1/3, 1/4]
+dphis = [1.0, 2.0, 3.0, 
+         1.0, 2.0,
+         1.0]
+# Since we add no noise here, we should perfectly recover the phi phase
+@test isapprox(A \ dphis, truth_phi[2:end], rtol=1e-7)
+
+v_hat = B \ dphis
+@test isapprox(v_hat, truth_v, rtol=1e-7)
+
+phi_hat = InsarTimeseries.integrate_velocities(v_hat, timediffs)
+@test isapprox(phi_hat, truth_phi, rtol=1e-7)
+
+
 
 # TODO: make the const. vel into a function to test it
