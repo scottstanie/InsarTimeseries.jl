004f347dabf594b24fd56b47877a173efae0f168
scott <scott.stanie@gmail.com>
2019-09-23 16:37:03 -0500

    add notes

--- src/InsarTimeseries.jl
+++ src/InsarTimeseries.jl
@@ -11,6 +11,18 @@ const sario = PyNULL()
 const gps = PyNULL()
 const utils = PyNULL()
 
+
+#TODO: functions needed to remove these python calls:
+# prepare.jl: a bunch of `sario`
+# gps.load_gps_los_data
+# scripts/find_abs_shift.jl:    lon, lat = gps.station_lonlat(station_name)
+# scripts/testl1.jl:  lon, lat = gps.station_lonlat(station_name)
+# src/analysis.jl: dts, gps_los_data = InsarTimeseries.gps.load_gps_los_data(geo_path, station_name,
+# src/prepare.jl: ref_row, ref_col = gps.station_rowcol(station_name=ref_station, rsc_data=rsc_data)
+# src/prepare.jl: geo_path = abspath(utils.get_parent_dir(igram_path))
+# src/prepare.jl: row_looks, col_looks = utils.find_looks_taken(igram_path, geo_path=geo_path)
+# srx/prepare.jl: utils.get_parent_dir(igram_path))
+# src/prepare.jl:    row_looks, col_looks = utils.find_looks_taken
 function __init__()
     copy!(sario, pyimport("apertools.sario"))
     copy!(gps, pyimport("apertools.gps"))
