9de267b2cbb0192ef9b86d4b16bb28daa3730296
scott <scott.stanie@gmail.com>
2019-09-30 13:28:53 -0500

    build B directly for speed

--- src/sbas.jl
+++ src/sbas.jl
@@ -183,6 +183,44 @@ function invert_pixel(pixel::AbstractArray{T, 1}, B::AbstractArray{T,2};
 end
 
 
+"""Takes the list of igram dates and builds the SBAS B (velocity coeff) matrix
+
+Each row corresponds to an igram, each column to a .geo
+Values will be t_k+1 - t_k for columns after the -1 in A,
+up to and including the +1 entry
+"""
+function build_B_matrix(geolist::Array{Date, 1}, intlist::Array{Igram, 1})
+
+    timediffs = day_diffs(geolist)
+    # We take the first .geo to be time 0, leave out of matrix
+    # Match on date (not time) to find indices
+    geolist2 = @view geolist[2:end]
+
+    M = length(intlist)  # Number of igrams, 
+    N = length(geolist2)
+
+    B = zeros(Float32, (M, N))
+
+    for i = 1:M
+        # row = A[i, :]
+        early_date, late_date = intlist[i]
+        idx_early = findfirst(isequal(early_date), geolist2)
+        if isnothing(idx_early)  # The first SLC will not be in the matrix
+            start_idx = 1
+        else
+            start_idx = idx_early + 1
+        end
+        idx_late = findfirst(isequal(late_date), geolist2)
+        end_idx = idx_late
+
+        # Now fill in the time diffs in the index range
+        B[i, start_idx:end_idx] .= @view timediffs[start_idx:end_idx]
+    end
+
+    return B
+end
+
+
 """
     build_A_matrix(geolist::Array{Date, 1}, intlist::Array{Igram, 1}) -> Array{Int8, 2}
 
@@ -213,45 +251,7 @@ function build_A_matrix(geolist::Array{Date, 1}, intlist::Array{Igram, 1})
     return A
 end
 
-"""Takes the list of igram dates and builds the SBAS B (velocity coeff) matrix
 
-Each row corresponds to an igram, each column to a .geo
-Values will be t_k+1 - t_k for columns after the -1 in A,
-up to and including the +1 entry
-"""
-function build_B_matrix(geolist::Array{Date, 1}, intlist::Array{Igram, 1})
-
-    A = build_A_matrix(geolist, intlist)
-    timediffs = day_diffs(geolist)
-    return _create_B(A, timediffs)
-end
-
-function build_B_matrix(A::Array{Int8, 2}, timediffs::Array{Int, 1})
-    return _create_B(A, timediffs)
-end
-
-function _create_B(A::Array{Int8, 2}, timediffs::Array{Int, 1})
-    B = zeros(Float32, size(A))
-
-    for i in 1:size(B, 1)
-        row = A[i, :]
-        start_idx = findfirst(row .== -1)
-        # if no -1 entry, start at index 1. Otherwise
-        if isnothing(start_idx)
-            start_idx = 1
-        else
-            start_idx += 1
-        end
-        # +1 will always exist in row
-        # End index is inclusive of the +1
-        end_idx = findfirst(row .== 1)
-
-        # Now fill in the time diffs in the index range
-        B[i, start_idx:end_idx] = timediffs[start_idx:end_idx]
-    end
-
-    return B
-end
 
 
 """For Tikhonov regularization, pad the B matrix with alpha*I"""
