db50a9a29a0461d260f3e88c3e281493bf5b3305
scott <scott.stanie@gmail.com>
2019-10-02 21:28:06 -0500

    extra switching old

--- scripts/run_map_combos.jl
+++ scripts/run_map_combos.jl
@@ -8,6 +8,8 @@ for (doprune, isl1, constvel, stackavg) in zip(prunes, L1arr, constant_vel, stac
     soltype = isl1 ? "l1" : (stackavg ? "stackavg" : "l2") 
     prunestr = doprune ? "prune" : "noprune"
     outfile = "velocities_$(prunestr)_$soltype.h5"
+    @show outfile
+
     try
         @time InsarTimeseries.run_inversion(constant_velocity=constvel,
                                         stack_average=stackavg,
@@ -19,4 +21,19 @@ for (doprune, isl1, constvel, stackavg) in zip(prunes, L1arr, constant_vel, stac
         println("Skipping $outfile")
         continue
     end
+
+    # stackavg && continue
+    # try
+    #     InsarTimeseries._save_std_attrs(outfile, "velos/1")
+    # catch
+    #     print()
+    # end
+    # for ds in ["stddev_raw/1", "stddev/1"]
+    #     tmp = h5read(outfile, ds)
+    #     h5open(outfile, "cw") do f
+    #         tmp = abs(InsarTimeseries.PHASE_TO_CM) .* read(f, ds)
+    #         o_delete(f[ds])
+    #         f[ds] = tmp
+    #     end
+    # end
 end
