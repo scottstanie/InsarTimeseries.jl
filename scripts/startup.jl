48ddd1e4fcdcdd2bc90df17de7721101292d100a
scott <scott.stanie@gmail.com>
2019-11-20 17:57:38 -0600

    do an import script

---
+++ scripts/startup.jl
@@ -0,0 +1,5 @@
+@time using HDF5
+@time import InsarTimeseries, MapImages, Sario
+@time include("/home/scott/repos/InsarTimeseries.jl/scripts/plotting.jl")
+@time include("/home/scott/repos/InsarTimeseries.jl/scripts/find_abs_shift.jl")
+@time include("/home/scott/repos/InsarTimeseries.jl/scripts/point_analysis.jl")
