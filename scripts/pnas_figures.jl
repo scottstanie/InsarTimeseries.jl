dcf30e792de2c046921ba1ce0df1850ea9bbbd51
scott <scott.stanie@gmail.com>
2020-01-08 15:48:05 -0600

    pnas stuff

--- scripts/pnas_figures.jl
+++ scripts/pnas_figures.jl
@@ -3,7 +3,239 @@ include("./plotting.jl")
 # Formatting functions:
 _remove_ticks(ax) = ax.tick_params(axis="both", which="both", bottom=false, top=false, labelbottom=false,
                                    left=false, right=false, labelleft=false)
-_set_figsize(fig, h=h, w=w) = (fig.set_figheight(h), fig.set_figwidth(w))
+_set_figsize(fig, h=3, w=3.5) = (fig.set_figheight(h), fig.set_figwidth(w))
+
+function pnas_outlier_figure(station_name="TXMC", outlier_color="r", h=3, w=3.5, bins=40)
+    # a. histogram before/after outlier
+    fig, ax = plt.subplots()
+
+    unw_vals = h5read("gps_pixels_78.h5", station_name)
+    geolist = Sario.load_geolist_from_h5("gps_pixels_78.h5");
+    intlist = Sario.load_intlist_from_h5("gps_pixels_78.h5");
+    end_date = Date(2018,1,1)
+    geolist18 = geolist[geolist .> end_date]
+    idxs = InsarTimeseries._good_idxs(geolist18, intlist)
+    intlist17 = intlist[idxs]
+    geolist17 = geolist[geolist .< end_date]
+    unw_vals17 = unw_vals[idxs]
+    vm = maximum(abs.(unw_vals17 * p2c))
+    n1, _, _ = ax.hist(unw_vals17.* p2c, range=(-vm, vm), bins=bins, label="All data", color="red")
+
+    means = InsarTimeseries.mean_abs_val(geolist17, intlist17, unw_vals17)
+    nsigma = 4
+    low, high = InsarTimeseries.two_way_cutoff(means, nsigma)
+    bigidx = means .> high 
+    idxs = InsarTimeseries._good_idxs(geolist17[bigidx], intlist17)
+    vals_after_outliers = unw_vals17[idxs]
+    n1, _, _ = ax.hist(vals_after_outliers .* p2c, range=(-vm, vm), bins=bins, label="Outliers removed", color="C0")
+    _remove_ticks(ax)
+    _set_figsize(fig)
+    fig.savefig("outliers_a.pdf", bbox_inches="tight", transparent=true, dpi=100)
+
+
+    # b) std dev of insar at GPS stations vs distance to reference
+    dists = [MapImages.latlon_to_dist(MapImages.station_latlon("TXKM"), MapImages.station_latlon(n)) for n in all_stations]
+
+    stds78 = Float64.(std.([h5read("gps_pixels_78.h5", name) for name in station_name_list78]))
+    dists78 = [MapImages.latlon_to_dist(MapImages.station_latlon("TXKM"), MapImages.station_latlon(n)) for n in station_name_list78]
+
+    stds85 = Float64.(std.([h5read("gps_pixels_85.h5", name) for name in station_name_list85]))
+    dists85 = [MapImages.latlon_to_dist(MapImages.station_latlon("TXKM"), MapImages.station_latlon(n)) for n in station_name_list85]
+    df_dists = DataFrame(dists=vcat(dists78, dists85), stds=vcat(stds78, stds85), names=(station_name_list78, station_name_list85))
+
+    sm = lm(@formula(stds ~ dists), df_dists)
+    a, b = coef(sm)
+    @show a, b*50
+
+    fig, ax = plt.subplots()
+    ax.scatter(df_dists[!, :dists], df_dists[!, :stds], s=40)
+    xs = 1:maximum(df_dists[!, :dists])
+    ax.plot(xs, a .+ b .* xs, lw=3)
+
+    fig.savefig("outliers_b.pdf", bbox_inches="tight", transparent=true, dpi=100)
+
+    # c) errors in 2, 3, 4 year solution at GPS vs distance, stacking only
+    # d) errors in 2, 3, 4 year solution at GPS vs distance, after outlier removal
+    df, errors = get_gps_errors(to_cm=true)
+    # df = @where(df, :station .!= "TXKM")
+
+    years = collect(2016:2018)
+    cols1 = [Symbol("p$(n)_stack_$(y)") for y=years, n=[78, 85]]
+    cols2 = [Symbol("p$(n)_outliers_$(y)") for y=years, n=[78, 85] ]
+    colors = ["b", "g", "k"]
+
+    maxy = 5
+    for cols in [cols1, cols2]
+        fig, ax = plt.subplots()
+        # errvals = abs.(vcat([df[!, c] for c in cols]...))
+        for yidx = 1:3
+            year = years[yidx]
+            c1, c2 = cols[yidx, :]
+            errvals = abs.(vcat(df[!, c1], df[!, c2]))
+            dists = repeat(df[!, :dist_to_txkm], 2)
+            dists = dists[.!isnan.(errvals)]
+            errvals = errvals[.!isnan.(errvals)]
+            @show maximum(errvals)
+
+            ax.scatter(dists, errvals, s=40, c=colors[yidx])
+            ax.set_ylim((0, maxy))
+
+            df1 = DataFrame(dist=dists, errval=errvals)
+            sm = lm(@formula(errval ~ dist), df1)
+            a, b = coef(sm)
+            @show a, b*50
+            ax.plot(xs, a .+ b .* xs, colors[yidx], lw=3)
+        end
+    end
+
+end
+
+
+import StatsBase: countmap
+import Images: label_components
+_max_label(labels) = sort([tup for tup in countmap(vec(labels)) if tup[1] != 0], by=x->x[2], rev=true)[1][1]
+
+function save_pnas_images(;years=[2016, 2017, 2018], 
+                          fnames=["velocities_$(year)_current.h5" for year in years],
+                          # fnames=["velocities_$(year)_linear_max800_sigma4.h5" for year in years],
+                          outdir=".",
+                          # zoomlats=(31.6, 30.9), zoomlons=(-103.8, -103.),  # Zoomed just to stripes
+                          zoomlats=(31.9, 31.0), zoomlons=(-103.8, -102.85),  # Covers txkm/txmh
+                          latbounds=(32.8, 30.7041),
+                          dset="velos_shifted/1", cmap1="seismic_wide_y", cmap2="seismic_wide_y", # cmap2=rdylbl, 
+                          vm1=10, vm2=10,
+                          dem_mask_height=nothing)
+    maxdays = maximum([_num_days(Sario.load_geolist_from_h5(f, dset)) for f in fnames])
+    @show maxdays
+    vm1 *= (maxdays / 3650)
+    vm2 *= (maxdays / 3650)
+    @show vm1, vm2 
+
+    m1, m2 = nothing, nothing
+    for f in fnames
+        @show f
+        days = _num_days(Sario.load_geolist_from_h5(f, dset))
+        @show days
+
+        m1 = MapImage(f, dset)
+        m1[m1 .== 0] .= NaN    
+
+        if !isnothing(dem_mask_height)
+            dem = Sario.load("elevation_looked.dem")
+            labels = label_components(dem .< 1200)
+            maxl = _max_label(labels)
+            m1[labels .!= maxl] .= NaN
+        end
+
+        m1 = m1[latbounds, (-110., -90.)] # no lon bounds really
+        @show MapImages.grid_extent(m1)
+
+        @show extremanan(m1)
+        m1 .*= (days / 3650)  # Cumulative, in cm
+        @show extremanan(m1)
+        m2 = m1[zoomlats, zoomlons]
+        out1 = joinpath(outdir, replace(f, ".h5"  => ".tif"))
+        # out2 = replace(f, ".h5"  => "_inset.tif")
+        out2 = joinpath(outdir, replace(f, ".h5"  => "_inset_gps.tif"))
+
+        # Save as both png and tif
+        save_img_geotiff(out1, m1, cmap=cmap1, vm=vm1)
+        # save_img_geotiff(out2, m2, cmap=cmap2, vm=vm2)
+        plt.imsave(replace(out1, ".tif" => ".png"), m1, cmap=cmap2, vmin=-vm2, vmax=vm2, dpi=150)
+        plt.imsave(replace(out2, ".tif" => ".png"), m2, cmap=cmap2, vmin=-vm2, vmax=vm2, dpi=150)
+    end
+    # Now save one with a colorbar
+    fig, axes = plt.subplots(1, 2)
+    axim = axes[1].imshow(m1, cmap=cmap1, vmax=vm1, vmin=-vm1)
+    fig.colorbar(axim, ax=axes[1])
+    axim = axes[2].imshow(m2, cmap=cmap2, vmax=vm2, vmin=-vm2)
+    fig.colorbar(axim, ax=axes[2])
+
+    cbar_name = "colorbars.png"
+    save_paper_figure(fig, cbar_name, false, 400)
+end
+
+
+function _errs_to_cm(df)
+    # mm/year, to cm for 2,3,4 years
+    scales = vcat(2 .* ones(4), 3 .* ones(4), 4.1 .* ones(4)) ./ 10
+    for (s, c) in zip(scales, names(df)[3:end])
+        df[!, c] = df[!, c] .* s
+    end 
+    return df
+end
+
+
+function get_gps_errors(;outfile="gps_errors.csv", to_cm=false)
+    if isfile(outfile)
+        df = CSV.read(outfile)
+        if to_cm
+            df = _errs_to_cm(df)
+        end
+        return df, [c for c in eachcol(df)][3:end]
+    end
+
+    p78 = "/data1/scott/pecos/path78-bbox2/igrams_looked/"
+    p85 = "/data4/scott/path85/stitched/igrams_looked/"
+
+   
+    dists = [MapImages.latlon_to_dist(MapImages.station_latlon("TXKM"), MapImages.station_latlon(n)) for n in all_stations]
+    errors = Any[]
+    push!(errors, all_stations)
+    push!(errors, dists)
+    # errors85_4 = get_gps_error(p85*"velocities_2018_current.h5", all_stations, dset="velos_shifted/1", verbose=true, window=7, shift=-0.)
+    # errors78_4 = get_gps_error(p78*"velocities_2018_current.h5", all_stations, dset="velos_shifted/1", verbose=true, window=7, shift=-0.)
+
+    for f in ["velocities_2018_stackavg_max800.h5", "velocities_2018_current.h5", 
+              "velocities_2017_stackavg_max800.h5", "velocities_2017_current.h5",
+              "velocities_2016_stackavg_max800.h5", "velocities_2016_current.h5",
+             ]
+
+        fname = p85*f; @show fname
+        push!(errors, round.(get_gps_error(fname, all_stations, dset="velos_shifted/1", verbose=false, window=7, shift=-0.), digits=2))
+        fname = p78*f; @show fname
+        push!(errors, round.(get_gps_error(fname, all_stations, dset="velos_shifted/1", verbose=false, window=7, shift=-0.), digits=2))
+    end
+    columns = [:station,
+               :dist_to_txkm,
+               :p85_stack_2018,
+               :p78_stack_2018,
+               :p85_outliers_2018,
+               :p78_outliers_2018,
+               :p85_stack_2017,
+               :p78_stack_2017,
+               :p85_outliers_2017,
+               :p78_outliers_2017,
+               :p85_stack_2016,
+               :p78_stack_2016,
+               :p85_outliers_2016,
+               :p78_outliers_2016,
+              ]
+
+    df = DataFrame(errors, columns)
+    CSV.write("gps_errors.csv", df)
+    return df, errors
+end
+
+function pnas_gps_error_table()
+    df, errors = get_gps_errors()
+    show(stdout, MIME("text/latex"), df)
+    @show extrema.(filter.(!isnan, errors[3:end]))
+    @show rms.(errors[3:end])
+    @show maximum.(filter.(!isnan, [abs.(a) for a in errors[3:end]]))
+    return df
+end
+
+function write_gps_pixels(station_names, max_temp=800)
+    geolist, intlist, valid_idxs = InsarTimeseries.load_geolist_intlist("unw_stack.h5", "geolist_ignore.txt", max_temp)
+    gpss = [get_stack_vals("unw_stack.h5", s, 3, "stack_flat_shifted", valid_idxs) for s in station_names]
+    for (idx, s) in enumerate(station_names)
+        h5write("gps_pixels.h5", s, gpss[idx])
+    end
+    Sario.save_intlist_to_h5("gps_pixels.h5", intlist)
+    Sario.save_geolist_to_h5("gps_pixels.h5", geolist)
+    Sario.save_dem_to_h5("gps_pixels.h5", Sario.load("dem.rsc"))
+end
 
 function pnas_outlier_figure_old(station_name="TXMC", outlier_color="r", h=3, w=3.5, bins=40)
 
@@ -116,159 +348,3 @@ function pnas_outlier_figure_old(station_name="TXMC", outlier_color="r", h=3, w=
     return (geolist17, intlist17, unw_vals17)
 end
 
-function pnas_outlier_figure()
-    dists = [MapImages.latlon_to_dist(MapImages.station_latlon("TXKM"), MapImages.station_latlon(n)) for n in all_stations]
-
-    stds78 = Float64.(std.([h5read("gps_pixels_78.h5", name) for name in station_name_list78]))
-    dists78 = [MapImages.latlon_to_dist(MapImages.station_latlon("TXKM"), MapImages.station_latlon(n)) for n in station_name_list78]
-
-    stds85 = Float64.(std.([h5read("gps_pixels_85.h5", name) for name in station_name_list85]))
-    dists85 = [MapImages.latlon_to_dist(MapImages.station_latlon("TXKM"), MapImages.station_latlon(n)) for n in station_name_list85]
-    df_dists = DataFrame(dists=vcat(dists78, dists85), stds=vcat(stds78, stds85), names=(station_name_list78, station_name_list85))
-
-    sm = lm(@formula(stds ~ dists), df_dists)
-    a, b = coef(sm)
-    @show a, b*50
-
-    fig, ax = plt.subplots()
-    ax.scatter(df_dists[!, :dists], df_dists[!, :stds], s=40)
-    xs = 1:maximum(df_dists[!, :dists])
-    ax.plot(xs, a .+ b .* xs, lw=3)
-
-    df, errors = get_gps_errors()
-end
-
-
-import StatsBase: countmap
-import Images: label_components
-_max_label(labels) = sort([tup for tup in countmap(vec(labels)) if tup[1] != 0], by=x->x[2], rev=true)[1][1]
-
-function save_pnas_images(;years=[2016, 2017, 2018], 
-                          fnames=["velocities_$(year)_current.h5" for year in years],
-                          # fnames=["velocities_$(year)_linear_max800_sigma4.h5" for year in years],
-                          outdir=".",
-                          # zoomlats=(31.6, 30.9), zoomlons=(-103.8, -103.),  # Zoomed just to stripes
-                          zoomlats=(31.9, 31.0), zoomlons=(-103.8, -102.85),  # Covers txkm/txmh
-                          latbounds=(32.8, 30.7041),
-                          dset="velos_shifted/1", cmap1="seismic_wide_y", cmap2="seismic_wide_y", # cmap2=rdylbl, 
-                          vm1=10, vm2=10,
-                          dem_mask_height=nothing)
-    maxdays = maximum([_num_days(Sario.load_geolist_from_h5(f, dset)) for f in fnames])
-    @show maxdays
-    vm1 *= (maxdays / 3650)
-    vm2 *= (maxdays / 3650)
-    @show vm1, vm2 
-
-    m1, m2 = nothing, nothing
-    for f in fnames
-        @show f
-        days = _num_days(Sario.load_geolist_from_h5(f, dset))
-        @show days
-
-        m1 = MapImage(f, dset)
-        m1[m1 .== 0] .= NaN    
-
-        if !isnothing(dem_mask_height)
-            dem = Sario.load("elevation_looked.dem")
-            labels = label_components(dem .< 1200)
-            maxl = _max_label(labels)
-            m1[labels .!= maxl] .= NaN
-        end
-
-        m1 = m1[latbounds, (-110., -90.)] # no lon bounds really
-        @show MapImages.grid_extent(m1)
-
-        @show extremanan(m1)
-        m1 .*= (days / 3650)  # Cumulative, in cm
-        @show extremanan(m1)
-        m2 = m1[zoomlats, zoomlons]
-        out1 = joinpath(outdir, replace(f, ".h5"  => ".tif"))
-        # out2 = replace(f, ".h5"  => "_inset.tif")
-        out2 = joinpath(outdir, replace(f, ".h5"  => "_inset_gps.tif"))
-
-        # Save as both png and tif
-        save_img_geotiff(out1, m1, cmap=cmap1, vm=vm1)
-        # save_img_geotiff(out2, m2, cmap=cmap2, vm=vm2)
-        plt.imsave(replace(out1, ".tif" => ".png"), m1, cmap=cmap2, vmin=-vm2, vmax=vm2, dpi=150)
-        plt.imsave(replace(out2, ".tif" => ".png"), m2, cmap=cmap2, vmin=-vm2, vmax=vm2, dpi=150)
-    end
-    # Now save one with a colorbar
-    fig, axes = plt.subplots(1, 2)
-    axim = axes[1].imshow(m1, cmap=cmap1, vmax=vm1, vmin=-vm1)
-    fig.colorbar(axim, ax=axes[1])
-    axim = axes[2].imshow(m2, cmap=cmap2, vmax=vm2, vmin=-vm2)
-    fig.colorbar(axim, ax=axes[2])
-
-    cbar_name = "colorbars.png"
-    save_paper_figure(fig, cbar_name, false, 400)
-end
-
-
-
-function get_gps_errors(outfile="gps_errors.csv")
-    if isfile(outfile)
-        df = CSV.read(outfile)
-        return df, [c for c in eachcol(df)][3:end]
-    end
-
-    p78 = "/data1/scott/pecos/path78-bbox2/igrams_looked/"
-    p85 = "/data4/scott/path85/stitched/igrams_looked/"
-
-   
-    dists = [MapImages.latlon_to_dist(MapImages.station_latlon("TXKM"), MapImages.station_latlon(n)) for n in all_stations]
-    errors = Any[]
-    push!(errors, all_stations)
-    push!(errors, dists)
-    # errors85_4 = get_gps_error(p85*"velocities_2018_current.h5", all_stations, dset="velos_shifted/1", verbose=true, window=7, shift=-0.)
-    # errors78_4 = get_gps_error(p78*"velocities_2018_current.h5", all_stations, dset="velos_shifted/1", verbose=true, window=7, shift=-0.)
-
-    for f in ["velocities_2018_stackavg_max800.h5", "velocities_2018_current.h5", 
-              "velocities_2017_stackavg_max800.h5", "velocities_2017_current.h5",
-              "velocities_2016_stackavg_max800.h5", "velocities_2016_current.h5",
-             ]
-
-        fname = p85*f; @show fname
-        push!(errors, round.(get_gps_error(fname, all_stations, dset="velos_shifted/1", verbose=false, window=7, shift=-0.), digits=2))
-        fname = p78*f; @show fname
-        push!(errors, round.(get_gps_error(fname, all_stations, dset="velos_shifted/1", verbose=false, window=7, shift=-0.), digits=2))
-    end
-    columns = [:station,
-               :dist_to_txkm,
-               :p85_stack_2018,
-               :p78_stack_2018,
-               :p85_outliers_2018,
-               :p78_outliers_2018,
-               :p85_stack_2017,
-               :p78_stack_2017,
-               :p85_outliers_2017,
-               :p78_outliers_2017,
-               :p85_stack_2016,
-               :p78_stack_2016,
-               :p85_outliers_2016,
-               :p78_outliers_2016,
-              ]
-
-    df = DataFrame(errors, columns)
-    CSV.write("gps_errors.csv", df)
-    return df, errors
-end
-
-function pnas_gps_error_table()
-    df, errors = get_gps_errors()
-    show(stdout, MIME("text/latex"), df)
-    @show extrema.(filter.(!isnan, errors[3:end]))
-    @show rms.(errors[3:end])
-    @show maximum.(filter.(!isnan, [abs.(a) for a in errors[3:end]]))
-    return df
-end
-
-function write_gps_pixels(station_names, max_temp=800)
-    geolist, intlist, valid_idxs = InsarTimeseries.load_geolist_intlist("unw_stack.h5", "geolist_ignore.txt", max_temp)
-    gpss = [get_stack_vals("unw_stack.h5", s, 3, "stack_flat_shifted", valid_idxs) for s in station_names]
-    for (idx, s) in enumerate(station_names)
-        h5write("gps_pixels.h5", s, gpss[idx])
-    end
-    Sario.save_intlist_to_h5("gps_pixels.h5", intlist)
-    Sario.save_geolist_to_h5("gps_pixels.h5", geolist)
-    Sario.save_dem_to_h5("gps_pixels.h5", Sario.load("dem.rsc"))
-end
